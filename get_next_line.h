de89d13749a8f2b79ccf0ded7797d1dad994321b
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-16 23:41:02 +0100

    start

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,18 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:27:52 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/16 17:27:52 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/16 23:40:51 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFFER_SIZE 32
+
+char	*get_next_line(int fd);
+
+#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,87 +1,12 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include "get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 17:27:56 by tcarlier          #+#    #+#             */
+/*   Updated: 2024/11/16 17:27:56 by tcarlier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
-	return (NULL);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*copy;
-	size_t	len = ft_strlen(s1);
-	size_t	i;
-
-	copy = (char *)malloc((len + 1) * sizeof(char));
-	if (!copy)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		copy[i] = s1[i];
-		i++;
-	}
-	copy[i] = '\0';
-	return (copy);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*joined;
-	size_t	len1 = ft_strlen(s1);
-	size_t	len2 = ft_strlen(s2);
-	size_t	i, j;
-
-	joined = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-	if (!joined)
-		return (NULL);
-	i = 0;
-	while (i < len1)
-	{
-		joined[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j < len2)
-	{
-		joined[i + j] = s2[j];
-		j++;
-	}
-	joined[i + j] = '\0';
-	return (joined);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*ptr;
-	size_t	total_size = count * size;
-	size_t	i;
-
-	ptr = malloc(total_size);
-	if (!ptr)
-		return (NULL);
-	i = 0;
-	while (i < total_size)
-	{
-		((unsigned char *)ptr)[i] = 0;
-		i++;
-	}
-	return (ptr);
-}
