c32ac09232572d0e055f59f88919fe7acae0a7a7
Tristan Carlier <tcarlier@c3r1p7.42perpignan.fr>
2024-11-17 17:54:47 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: tcarlier <tcarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:27:54 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/17 17:25:33 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/17 17:54:17 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 char	*get_next_line(int fd)
 {
 	static t_lst	*lst;
-	char			*res;
 	int				i;
 	int				c;
+	char			*res;
 	
 	if (fd == 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -25,7 +25,7 @@ char	*get_next_line(int fd)
 		ft_lst_init(&lst, fd);
 	c = ft_find_fd(lst, fd);
 	i = -1;
-	while(ft_new_line(lst->buff[c / 2]) != 0 || i != 0)
+	while(ft_new_line(lst->buff[c / 2]) != 0 || i == 0)
 	{
 		lst->buff[c / 2] = ft_realloc(lst->buff[c / 2], ft_strlen(lst->buff[c / 2]),
 			ft_strlen(lst->buff[c / 2]) + BUFFER_SIZE);
@@ -33,9 +33,10 @@ char	*get_next_line(int fd)
 	}
 	lst->tab[c + 1] = i + 1;
 	if (i == 0)
-		return (ft_substr(lst->buff[c / 2], lst->tab[c + 1], ft_strlen(lst->buff[c / 2])));
+		res = (ft_substr(lst->buff[c / 2], lst->tab[c + 1], ft_strlen(lst->buff[c / 2])));
 	else
-		return (ft_substr(lst->buff[c / 2], lst->tab[c + 1], ft_s(lst->buff[c / 2], '\n') + 1));
+		res = (ft_substr(lst->buff[c / 2], lst->tab[c + 1], ft_s(lst->buff[c / 2], '\n') + 1));
+	lst->buff[c / 2] = ft_substr(lst->buff[c / 2], ft_strlen(res), ft_strlen(lst->buff[c / 2]));
 	return (res);
 }
 char	*ft_substr(char *s, unsigned int start, size_t len)
@@ -68,13 +69,11 @@ int	ft_s(const char *s, int c)
 	while (s[i])
 	{
 		if (s[i] == (char)c)
-			res = i;
+			return (i);
 		i++;
 	}
 	if ((char)c == '\0')
 		return ((int)ft_strlen((char *)s));
-	if (res == 0 && s[0] != (char)c)
-		return (0);
 	return (res);
 }
 size_t	ft_strlen(char *str)
@@ -114,10 +113,12 @@ int	main(void)
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
 		return (0);
-	while ((line = get_next_line(fd)))
+	line = get_next_line(fd);
+	while ((line))
 	{
 		printf("%s", line);
 		free(line);
+		line = get_next_line(fd);
 	}
 	close(fd);
 	return (0);
