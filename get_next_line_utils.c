4e4db0d88dd2fc0b713c03dfeac1ea5346f7d3fe
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-22 22:46:28 +0100

    update

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:27:56 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/22 22:29:00 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/22 22:46:18 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,79 +16,79 @@
 
 size_t	ft_strlen(const char *s)
 {
-	size_t	i;
+    size_t	i;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
 }
 
 char	*ft_strdup(const char *s)
 {
-	char	*dup;
-	size_t	i;
-	size_t	len;
+    char	*dup;
+    size_t	i;
+    size_t	len;
 
-	len = ft_strlen(s);
-	dup = malloc(len + 1);
-	if (!dup)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
+    len = ft_strlen(s);
+    dup = malloc(len + 1);
+    if (!dup)
+        return (NULL);
+    i = 0;
+    while (i < len)
+    {
+        dup[i] = s[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
 }
 
 char	*ft_substr(char *s, unsigned int start, size_t len)
 {
-	char	*str;
-	size_t	i;
+    char	*str;
+    size_t	i;
 
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	str = malloc(len + 1);
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (i < len && s[start + i])
-	{
-		str[i] = s[start + i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+    if (!s)
+        return (NULL);
+    if (start >= ft_strlen(s))
+        return (ft_strdup(""));
+    str = malloc(len + 1);
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (i < len && s[start + i])
+    {
+        str[i] = s[start + i];
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
 }
 
 void	ft_init(t_gnl *f)
 {
-	if (!f)
-		return ;
-	if (!(*f).buf)
-	{
-		(*f).buf = ft_strdup("");
-		(*f).tab = 0;
-		(*f).eof = 1;
-		(*f).c = 0;
-	}
+    if (!f)
+        return;
+    if (!f->buf)
+    {
+        f->buf = ft_strdup("");
+        f->tab = 0;
+        f->eof = 0;
+        f->c = 0;
+    }
 }
 
 void	cleanup_fd(t_gnl *f)
 {
-	if (!f)
-		return ;
-	if (f->buf)
-	{
-		free(f->buf);
-		f->buf = NULL;
-	}
-	f->tab = 0;
-	f->eof = 0;
-	f->c = -1;
+    if (!f)
+        return;
+    if (f->buf)
+    {
+        free(f->buf);
+        f->buf = NULL;
+    }
+    f->tab = 0;
+    f->eof = 0;
+    f->c = -1;
 }
