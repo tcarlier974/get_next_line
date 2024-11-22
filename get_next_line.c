232f88f611fbffa0094ff144a505feb6cfdbee7f
Tristan Carlier <tristan@MacBook-Pro-de-Tristan.local>
2024-11-22 18:59:02 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:27:54 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/11/22 18:57:58 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/11/22 18:58:59 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,6 +135,9 @@ int	main(void)
 		printf("%s", line);
 		free(line);
 	}
+	line = get_next_line(fd1);
+	printf("%s", line);
+	free(line);
 	close(fd1);
 	free(line);
 	return (0);
